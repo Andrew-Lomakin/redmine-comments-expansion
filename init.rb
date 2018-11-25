ca8ce6eec57c4ab9ba77db89c9bee1a5cea201b3
Andrew Lomakin <lomtik4life@gmail.com>
2018-11-25 15:14:05 +0400

    fix

--- init.rb
+++ init.rb
@@ -1,8 +1,8 @@
-Redmine::Plugin.register :comment_exp do
-  name 'Comment Exp plugin'
+Redmine::Plugin.register 'redmine-comments-expansion' do
+  name 'redmine:Comments expansion'
   author 'Andrew Lomakin'
   description 'Comment expansion'
   version '0.0.1'
-  url 'none'
-  author_url 'none'
+  url 'https://github.com/Andrew-Lomakin/redmine-comments-expansion'
+  author_url 'https://github.com/Andrew-Lomakin'
 end
