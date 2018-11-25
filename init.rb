a700968666a7f55eb5bc8658b5f3111f9fca28e4
Andrew Lomakin <lomtik4life@gmail.com>
2018-11-25 14:48:56 +0400

    lslsls

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :comment_exp do
+  name 'Comment Exp plugin'
+  author 'Andrew Lomakin'
+  description 'Comment expansion'
+  version '0.0.1'
+  url 'none'
+  author_url 'none'
+end
