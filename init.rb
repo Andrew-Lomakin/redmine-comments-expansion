d6d06545f51a5d89eee96ab09298f687661d23b8
Andrew Lomakin <lomtik4life@gmail.com>
2018-11-29 15:26:25 +0400

    rename

--- init.rb
+++ init.rb
@@ -1,8 +1,8 @@
-Redmine::Plugin.register 'redmine-comments-expansion' do
+Redmine::Plugin.register 'redmine_comments_expansion' do
   name 'redmine:Comments expansion'
   author 'Andrew Lomakin'
   description 'Comment expansion'
-  version '0.0.1'
-  url 'https://github.com/Andrew-Lomakin/redmine-comments-expansion'
+  version '0.0.2'
+  url 'https://github.com/Andrew-Lomakin/redmine_comments_expansion'
   author_url 'https://github.com/Andrew-Lomakin'
 end
